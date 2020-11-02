9562d831d40df31a485634d684978a469c69b2c2
bami <1992bami@gmail.com>
2020-11-02 18:54:04 +0900

    Using Array

--- app.ts
+++ app.ts
@@ -1,9 +1,18 @@
-const person: {
-  name: string;
-  age: number;
-} = {
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+const person = {
   name: 'Maximilian',
-  age: 30
+  age: 30,
+  hobbies: ['Sports', 'Cooking']
 };
 
+let favoriteActivities: any[];
+favoriteActivities = ['Sports', 1];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
