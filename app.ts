f2150e96557f10c614f9dc65b07df9f6025f013b
bami <1992bami@gmail.com>
2020-11-02 19:09:26 +0900

    Using Enum

--- app.ts
+++ app.ts
@@ -1,14 +1,27 @@
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
-} = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: 'Maximilian',
+//   age: 30,
+//   hobbies: ['Sports', 'Cooking'],
+//   role: [2, 'author']
+// };
+
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role { ADMIN = 'ADMIN', READ_ONLY = 100, AUTHOR = 'AUTHOR' };
+
+const person = {
   name: 'Maximilian',
   age: 30,
   hobbies: ['Sports', 'Cooking'],
-  role: [2, 'author']
-};
+  role: Role.ADMIN
+}
 
 // person.role.push('admin'); // pushはエラー検知されない
 // person.role[1] = 10; // エラー検知される
@@ -22,4 +35,8 @@ console.log(person.name);
 
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
+}
+
+if (person.role === Role.AUTHOR) {
+  console.log('is author');
 }
