402a77a0cd83959eda526622d1109fa621e0b3ae
bami <1992bami@gmail.com>
2020-11-03 01:51:13 +0900

    Function Type and Callbacks

--- app.ts
+++ app.ts
@@ -6,6 +6,11 @@ const printResult = (num: number): void => {
   console.log('Result: ' + num);
 }
 
+const addAndHandle = (n1: number, n2: number, cb: (num: number) => void) => {
+  const result = n1 + n2;
+  cb(result);
+}
+
 printResult(add(5, 12));
 
 let combinedValues: (a: number, b: number) => number;
@@ -15,3 +20,7 @@ combinedValues = add;
 // combinedValues = 5;
 
 console.log(combinedValues(8, 8));
+
+addAndHandle(10, 20, result => {
+  console.log(result);
+});
