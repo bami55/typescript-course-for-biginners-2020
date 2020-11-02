a4f6c97400ad7c3c57baf10c9e1da713e7210c38
bami <1992bami@gmail.com>
2020-11-03 01:55:27 +0900

    Unknown Type

--- app.ts
+++ app.ts
@@ -1,26 +1,8 @@
-const add = (n1: number, n2: number) => {
-  return n1 + n2;
-};
+let userInput: unknown;
+let userName: string;
 
-const printResult = (num: number): void => {
-  console.log('Result: ' + num);
+userInput = 5;
+userInput = 'Max';
+if (typeof userInput === 'string') {
+  userName = userInput;
 }
-
-const addAndHandle = (n1: number, n2: number, cb: (num: number) => void) => {
-  const result = n1 + n2;
-  cb(result);
-}
-
-printResult(add(5, 12));
-
-let combinedValues: (a: number, b: number) => number;
-
-combinedValues = add;
-// combinedValues = printResult;
-// combinedValues = 5;
-
-console.log(combinedValues(8, 8));
-
-addAndHandle(10, 20, result => {
-  console.log(result);
-});
