774d00922db307fa863c94b80b7977e35be073ae
bami <1992bami@gmail.com>
2020-11-02 19:00:27 +0900

    Using Tuple

--- app.ts
+++ app.ts
@@ -1,12 +1,19 @@
-// const person: {
-//   name: string;
-//   age: number;
-// } = {
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
   name: 'Maximilian',
   age: 30,
-  hobbies: ['Sports', 'Cooking']
+  hobbies: ['Sports', 'Cooking'],
+  role: [2, 'author']
 };
+
+// person.role.push('admin'); // pushはエラー検知されない
+// person.role[1] = 10; // エラー検知される
+
+// person.role = [0, 'admin', 'user'];  // エラー検知される
 
 let favoriteActivities: any[];
 favoriteActivities = ['Sports', 1];
